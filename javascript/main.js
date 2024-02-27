e9b1b4bf2de2d995c5847a5d66b70e9d654ba45a
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-27 13:32:40 +0100

    navbar

---
+++ javascript/main.js
@@ -0,0 +1,23 @@
+const openNavIcon =  document.querySelector(".open__nav-btn")
+const closeNavIcon =  document.querySelector(".close__nav-btn")
+const navItems = document.querySelectorAll(".nav__details")
+
+
+
+openNavIcon.addEventListener("click", () => {
+     navItems.forEach((navItem)=>{
+       navItem.style.display = "flex";
+     })
+     openNavIcon.classList.add("hidden")
+     closeNavIcon.classList.remove("hidden")
+});
+
+closeNavIcon.addEventListener("click", () =>{
+    navItems.forEach((navItem)=>{
+        navItem.style.display = "none";
+      })
+      openNavIcon.classList.remove("hidden")
+      closeNavIcon.classList.add("hidden")
+}
+
+)
