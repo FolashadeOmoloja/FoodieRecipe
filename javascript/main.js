de5060e9c67bbb9acbfc904f42eb68f8a5d8ae32
FolashadeOmoloja <omolojashade@gmail.com>
2024-03-04 15:58:22 +0100

    sign up

--- javascript/main.js
+++ javascript/main.js
@@ -7,24 +7,68 @@
 
 // for the navbar control
 
-const openNavIcon =  document.querySelector(".open__nav-btn")
-const closeNavIcon =  document.querySelector(".close__nav-btn")
-const navItems = document.querySelectorAll(".nav__details")
+const openNavIcon =  document.querySelector(".open__nav-btn"),
+closeNavIcon =  document.querySelector(".close__nav-btn"),
+navItems = document.querySelectorAll(".nav__details"),
+recipeSeekerRole =  document.querySelector(".form-role-seeker"),
+chefRole =  document.querySelector(".form-role-chef"),
+adminRole =  document.querySelector(".form-role-admin"),
+viewPasswordIcon = document.querySelectorAll('.view-password'),
+hidePasswordIcon = document.querySelectorAll('hide-password')
 
 
 
-openNavIcon? openNavIcon.addEventListener("click", () => {
+
+openNavIcon ? openNavIcon.addEventListener("click", () => {
      navItems.forEach((navItem)=>{
        navItem.style.display = "flex";
      })
      openNavIcon.classList.add("hidden")
      closeNavIcon.classList.remove("hidden")
-}): null;
+}) : null;
 
-closeNavIcon? closeNavIcon.addEventListener("click", () =>{
+closeNavIcon ? closeNavIcon.addEventListener("click", () =>{
     navItems.forEach((navItem)=>{
         navItem.style.display = "none";
       })
       openNavIcon.classList.remove("hidden")
       closeNavIcon.classList.add("hidden")
 }): null;
+
+
+//password
+viewPasswordIcon? viewPasswordIcon.forEach((viewIcon, idx)=>{
+  viewIcon.addEventListener("click", () =>{
+    console.log(viewPasswordIcon[0])
+    console.log(viewPasswordIcon[1])
+    viewIcon.style.display = 'none'
+    // hidePasswordIcon[idx].classList.remove('hidden')
+  })
+}) : null
+
+
+
+//Choosing Role on sign up
+const selectRole = (num) =>{
+   if (num === 0) {
+     recipeSeekerRole.classList.add("role__cont__select")
+     chefRole.classList.remove("role__cont__select")
+     adminRole.classList.remove("role__cont__select")
+   } 
+   else if(num === 1){
+      chefRole.classList.add("role__cont__select")
+      recipeSeekerRole.classList.remove("role__cont__select")
+      adminRole.classList.remove("role__cont__select")}
+   else if (num === 2) {
+      adminRole.classList.add("role__cont__select")
+      recipeSeekerRole.classList.remove("role__cont__select")
+      chefRole.classList.remove("role__cont__select")
+    } else{
+      null
+    }
+   }
+
+recipeSeekerRole ? recipeSeekerRole.addEventListener("click",()=>selectRole(0) ) : null
+chefRole ? chefRole.addEventListener("click",()=>selectRole(1) ) : null
+adminRole ?adminRole.addEventListener("click",()=>selectRole(2) ) : null
+
